ff29787c27e2c98b6efd9ec11dd55fcbf29e8967
SirMathhman <sirmathhman@gmail.com>
2019-10-31 22:27:53 -0400

    Clean from inspections

--- src/main/java/com/meti/BlockTokenBuilder.java
+++ src/main/java/com/meti/BlockTokenBuilder.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 class BlockTokenBuilder implements TokenBuilder {
 	@Override
 	public Optional<Token> build(LexerState lexerState) {
+		Optional<Token> result;
 		String value = lexerState.compute();
-		if(value.equals("={")){
-			return Optional.of(new BlockToken());
-		} else if(value.equals("}")) {
-			return Optional.of(new BlockToken());
+		if (value.equals("={")) {
+			result = Optional.of(new BlockToken());
+		} else if (value.equals("}")) {
+			result = Optional.of(new BlockToken());
+		} else {
+			result = Optional.empty();
 		}
-		return Optional.empty();
+		return result;
 	}
 }
