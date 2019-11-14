8355eb6a6dbde1d3b60b79b29cb0ae8669bbcbdb
SirMathhman <sirmathhman@gmail.com>
2019-11-14 14:12:47 -0500

    Fix matcher.build(State, Class<T>) implementation

--- src/main/java/com/meti/lexeme/match/Matcher.java
+++ src/main/java/com/meti/lexeme/match/Matcher.java
@@ -5,14 +5,13 @@ import com.meti.lexeme.LexerState;
 import java.util.Optional;
 
 public interface Matcher {
-    Optional<Match<?>> build(LexerState state);
+	default <T extends Match<?>> Optional<T> build(LexerState state, Class<T> clazz) {
+		try {
+			return build(state).map(clazz::cast);
+		} catch (Exception e) {
+			return Optional.empty();
+		}
+	}
 
-    default <T extends Match<?>> Optional<T> build(LexerState state, Class<T> clazz) {
-        var result = build(state);
-        try {
-            return Optional.ofNullable(clazz.cast(result));
-        } catch (ClassCastException e) {
-            return Optional.empty();
-        }
-    }
+	Optional<Match<?>> build(LexerState state);
 }
