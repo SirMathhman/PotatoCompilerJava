afc3396def88ba9aa852c43ccd922076b580b15d
SirMathhman <sirmathhman@gmail.com>
2019-11-22 17:56:44 -0500

    Move test to correct package

--- src/test/java/com/meti/pipeline/FunctionTest.java
+++ src/test/java/com/meti/pipeline/FunctionTest.java
@@ -1,11 +1,11 @@
-package com.meti.lex.token;
+package com.meti.pipeline;
 
 import com.meti.PotatoTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class FunctionTokenizeTest extends PotatoTest {
+class FunctionTest extends PotatoTest {
 	@Test
 	void test() {
 		assertEquals("function a0(){}", compile("empty={}"));
