46710121a043b987df2fc573ab19f4ab8378fdb9
SirMathhman <sirmathhman@gmail.com>
2019-11-23 13:17:17 -0500

    Add RETURN to Keyword

--- src/main/java/com/meti/lex/token/Keyword.java
+++ src/main/java/com/meti/lex/token/Keyword.java
@@ -1,16 +1,17 @@
 package com.meti.lex.token;
 
 public enum Keyword implements Valued<String> {
-    IF("if");
+    IF("if"),
+	RETURN("return");
 
-    private final String value;
+	private final String value;
 
-    Keyword(String value) {
-        this.value = value;
-    }
+	Keyword(String value) {
+		this.value = value;
+	}
 
-    @Override
-    public String value() {
-        return value;
-    }
+	@Override
+	public String value() {
+		return value;
+	}
 }
